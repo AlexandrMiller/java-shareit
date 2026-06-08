2257c68d3b0343520b6ff75781af5184fd888c29
AlexandrMiller <miller.alex.rob@yandex.ru>
2026-06-08 18:18:47 +0500

    sprint16.4

--- gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.booking.client.BookingClient;
+import ru.practicum.shareit.booking.BookingClient;
 import ru.practicum.shareit.booking.controller.BookingController;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 
